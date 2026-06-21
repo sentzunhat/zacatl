53f5c70c5fe15ef9828fd2c936bbcfd3f7591900
Diego Beltran <beltrd@gmail.com>
2026-06-21 12:29:24 -0500

    remove unused third-party dependencies

--- src/third-party/sqlite3.ts
+++
@@ -1,4 +0,0 @@
-import sqlite3 from 'sqlite3';
-
-export { sqlite3 };
-export default sqlite3;
--- src/third-party/tsyringe.ts
+++
@@ -1,8 +0,0 @@
-/**
- * tsyringe exports
- * Centralized in third-party folder to isolate external integrations.
- */
-export { container, singleton, inject } from 'tsyringe';
-
-// Type-only exports
-export type { DependencyContainer, InjectionToken } from 'tsyringe';
